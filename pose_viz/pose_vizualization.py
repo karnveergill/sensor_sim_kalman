a5ecc294ced1e5beeafa02fd38c2d17ffb973046
karnveergill <karnveerg94@gmail.com>
2025-02-25 08:21:04 +0000

    converting lat/lon to UTM and offsetting to center of screen

--- pose_viz/pose_vizualization.py
+++ pose_viz/pose_vizualization.py
@@ -4,17 +4,20 @@ import rclpy
 from rclpy.node import Node
 from PyQt5.QtWidgets import QApplication, QGraphicsView, QGraphicsScene, QGraphicsRectItem, QApplication, QMainWindow
 from PyQt5.QtCore import QTimer, Qt
-from PyQt5.QtGui import QPainter
+from PyQt5.QtGui import QPainter, QBrush, QColor
 from geometry_msgs.msg import PointStamped
 from geometry_msgs.msg import PoseStamped
 from sensor_msgs.msg import NavSatFix
 from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy
+from pyproj import Proj, Transformer
 
 # Constants
 ROS2_QOS_CPP_PROFILE = QoSProfile(reliability=QoSReliabilityPolicy.RELIABLE,  
                                   history=QoSHistoryPolicy.KEEP_LAST,
                                   depth=10)
-LATLON_TO_WINDOW_SCALE = 10
+EASTING_OFFSET = 737686.00
+NORTHING_OFFSET = 4206112.06
+
 
 ###############################################################################
 # GPSVisualizer Class Definition
@@ -51,12 +54,10 @@ class GPSVisualizer(Node, QMainWindow):
         
         # Create two rectangles for GPS and filtered data
         self.gps_box = QGraphicsRectItem(-5, -5, 10, 10)
-        self.gps_box.setBrush(Qt.red)
-        self.gps_box.setOpacity(0.6)
+        self.gps_box.setBrush(QBrush(QColor(255, 0, 0, 150)))
         
         self.filtered_box = QGraphicsRectItem(-5, -5, 10, 10)
-        self.filtered_box.setBrush(Qt.blue)
-        self.filtered_box.setOpacity(0.6)
+        self.filtered_box.setBrush(QBrush(QColor(0, 0, 255, 150)))
         
         # Add created items to QT scene
         self.scene.addItem(self.gps_box)
@@ -66,20 +67,23 @@ class GPSVisualizer(Node, QMainWindow):
         self.timer = QTimer()
         self.timer.timeout.connect(self.update_scene)
         self.timer.start(50) # Update every 50ms (20Hz)
+
+        # Initialize UTM projections for lat/lon covertions
+        self.utm_proj = Proj(proj="utm", zone=11, ellps="WGS84", datum="WGS84")
     
     ###########################################################################
     # GPS Data Callback
     def gps_callback(self, msg):
         # print("Receieved GPS Pose Data")
-        self.gps_pos = [msg.latitude * LATLON_TO_WINDOW_SCALE, 
-                        -msg.longitude * LATLON_TO_WINDOW_SCALE]
+        x, y = self.utm_proj(msg.longitude, msg.latitude)
+        self.gps_pos = [x - EASTING_OFFSET, y - NORTHING_OFFSET]
 
     ###########################################################################
     # Kalman Filter Data Callback 
     def filtered_callback(self, msg):
         # print("Received Kalman Filter Pose Data")
-        self.filtered_pos = [msg.pose.position.x * LATLON_TO_WINDOW_SCALE, 
-                             -msg.pose.position.y * LATLON_TO_WINDOW_SCALE]
+        x, y = self.utm_proj(msg.pose.position.y, msg.pose.position.x)
+        self.filtered_pos = [x - EASTING_OFFSET, y - NORTHING_OFFSET]
 
     ###########################################################################
     # Function to update QT scene with visuals 
@@ -102,7 +106,7 @@ def ros_spin(node):
     rclpy.spin(node)
 
 ###############################################################################
-# Application main to create visualizer node and run it
+# Application main to create visualizer node thread and start Qt window
 def main():
     rclpy.init()
     app = QApplication(sys.argv)
